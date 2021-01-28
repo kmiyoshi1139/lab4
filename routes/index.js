b6a8b501fe440460e4412daba56f67259a8eed52
Kazuha Miyoshi <kmiyoshi@ucsd.edu>
2021-01-28 11:20:54 -0800

    node.js lab

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,24 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render("index", {
+  	"projects": [
+  		{ "name": "Waiting in Line", 
+  	 	  "image": "lorempixel.people.1.jpeg",
+  	 	  "id": "project1"
+  		},
+		{ "name": "Needfinding",
+  		  "image": "lorempixel.city.1.jpeg",
+  	 	  "id": "project2"
+  		},
+  		{ "name": "Protoyping",
+  		  "image": "lorempixel.abstract.8.jpeg",
+  	 	  "id": "project2"
+  		},
+  		{ "name": "Heuristic Evaluation",
+  		  "image": "lorempixel.people.2.jpeg",
+  	 	  "id": "project2"
+  		},
+  	] 
+  });
 };
